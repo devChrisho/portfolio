0daf70a5930c74bec6f97eff151e33825c02e74e
ChrisHo <68413782+devChrisho@users.noreply.github.com>
2024-05-02 16:52:38 +0800

    chore: organised test folder structure

--- src/tests/testSuites/App.test.tsx
+++ src/tests/testSuites/App.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
-import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "../../redux/store";
+import App from "../../App";
 
 describe("App unit test", () => {
   it("should pass snapshot", () => {
